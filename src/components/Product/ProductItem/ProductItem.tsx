ee7cc4cb9f784b699b90a4dd1bd7aaf87a1fde82
Adam Foster <adam.foster@cleancoqld.com.au>
2021-08-17 11:00:38 +1000

    fix product images

--- src/components/Product/ProductItem/ProductItem.tsx
+++ src/components/Product/ProductItem/ProductItem.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { Product } from '../Product.type'
 import Image from 'next/image'
 import styles from '../../../styles/Products.module.css'
+import { background } from '@storybook/theming'
 
 interface ProductItemProps {
   product: Product
@@ -19,14 +20,14 @@ export const ProductItem: React.FC<ProductItemProps> = ({
     <Box className={styles['product__item']} onClick={() => setProduct(product)}>
       <Box className={styles['product__item--content']}>
         <Box className={styles['product__item--content--image']}>
-          <Box style={{width:'200px', height: '100%'}}>
-            <Image
-              // className={classes.productImage}
+          <Box style={{width:'200px', height: '100%', backgroundColor:'red'}}>
+            <Box style={{background:`transparent url(${product.attributes.e_image_urls_search_jpg[0][0]}) no-repeat 50% 50%`, width:'100%', height:'100%'}}></Box>
+            {/* <Image
               src={product?.attributes?.e_image_urls_search_jpg[0][0]}
               alt={product.attributes.product_name}
-              width={'100%'}
-              height={'100%'}
-            />
+              width={250}
+              height={'auto'}
+            /> */}
           </Box>
         </Box>
         <Box className={styles['product__item__title']}>
