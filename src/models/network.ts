58175f4d65395e97a7a3e3cd75f8f798e25238dd
Adam Foster <adam.foster@cleancoqld.com.au>
2021-08-17 12:17:45 +1000

    comments

--- src/models/network.ts
+++ src/models/network.ts
@@ -1,17 +1,20 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from 'axios'
 
 export const getProducts = async (param: any[]): Promise<AxiosResponse> => {
-  let paramString = "";
+  // define param string
+  let paramString = '?'
 
+  // add params to request
   param.map((keyVal: any, i: number) => {
-    if (i > 0) paramString += "&";
-    Object.keys(keyVal).map(
-      (k: string) => (paramString += `${k}=${keyVal[k]}`)
-    );
-  });
+    if (i > 0) paramString += '&'
+    Object.keys(keyVal).map((k: string) => (paramString += `${k}=${keyVal[k]}`))
+  })
 
+  // request data with params if present
   const response = await axios.get(
-    `https://api.theurge.com.au/search-results?${paramString}`
-  );
-  return response;
-};
+    `https://api.theurge.com.au/search-results${
+      param.length > 0 ? paramString : ''
+    }`
+  )
+  return response
+}
