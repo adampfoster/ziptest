c4a70b47f61803847256af9ef19231316f959f92
Adam Foster <adam.foster@cleancoqld.com.au>
2021-08-17 13:02:04 +1000

    update type

--- src/components/Product/Product.type.ts
+++ src/components/Product/Product.type.ts
@@ -8,7 +8,7 @@ export interface FriendlyAlternates {
 
 export interface Attributes {
   availability: string
-  color: string
+  color?: string
   converted_currency: string
   converted_retailer_price: number
   converted_sale_price: number
@@ -21,6 +21,8 @@ export interface Attributes {
   e_categories_path: string[]
   e_color: string
   e_color_parent: string
+  e_free_shipping_currency?: string
+  e_free_shipping_over?: number[]
   e_friendly_id: string
   e_friendly_ids: string[]
   e_gender_list: string[]
@@ -32,16 +34,22 @@ export interface Attributes {
   e_image_urls_search_webp: string[][]
   e_is_free_returns: boolean
   e_is_free_shipping: boolean
-  e_is_on_sale: boolean
+  e_is_on_sale?: boolean
   e_item_code: string
-  e_material: string
+  e_material?: string
+  e_payment_options?: string[]
   e_product_name: string
   e_retailer_display_domain: string
   e_retailer_display_name: string
   e_retailer_facet_name: string
   e_retailer_industry: string
   e_retailer_short_id: string
-  e_sales: string[]
+  e_retailer_type?: string
+  e_returns_link?: string
+  e_returns_period?: number[]
+  e_sales?: string[]
+  e_shipping_link?: string
+  e_shipping_deals?: string[]
   e_subcat: string[]
   friendly_alternates: FriendlyAlternates
   friendly_canonical: string
@@ -49,13 +57,14 @@ export interface Attributes {
   hreflangs: string[]
   item_code: string
   long_description: string
+  material?: string
   product_name: string
   retailer_code: string
   retailer_price: number
   retailer_url: string
-  sale_price: number
+  sale_price?: number
   sku_code: string
-  updated_at: Date
+  updated_at: Date | string
 }
 
 export interface Product {
