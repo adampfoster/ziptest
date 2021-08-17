b90ce5f7c381e6f2b7f0910975266837f09b352a
Adam Foster <adam.foster@cleancoqld.com.au>
2021-08-17 11:02:54 +1000

    remove red

--- src/components/Product/ProductItem/ProductItem.tsx
+++ src/components/Product/ProductItem/ProductItem.tsx
@@ -1,6 +1,4 @@
-import {
-  Box, makeStyles,
-} from '@material-ui/core'
+import { Box, makeStyles } from '@material-ui/core'
 import React from 'react'
 import { Product } from '../Product.type'
 import Image from 'next/image'
@@ -17,17 +15,20 @@ export const ProductItem: React.FC<ProductItemProps> = ({
   setProduct,
 }) => {
   return (
-    <Box className={styles['product__item']} onClick={() => setProduct(product)}>
+    <Box
+      className={styles['product__item']}
+      onClick={() => setProduct(product)}
+    >
       <Box className={styles['product__item--content']}>
         <Box className={styles['product__item--content--image']}>
-          <Box style={{width:'200px', height: '100%', backgroundColor:'red'}}>
-            <Box style={{background:`transparent url(${product.attributes.e_image_urls_search_jpg[0][0]}) no-repeat 50% 50%`, width:'100%', height:'100%'}}></Box>
-            {/* <Image
-              src={product?.attributes?.e_image_urls_search_jpg[0][0]}
-              alt={product.attributes.product_name}
-              width={250}
-              height={'auto'}
-            /> */}
+          <Box style={{ width: '200px', height: '100%' }}>
+            <Box
+              style={{
+                background: `transparent url(${product.attributes.e_image_urls_search_jpg[0][0]}) no-repeat 50% 50%`,
+                width: '100%',
+                height: '100%',
+              }}
+            ></Box>
           </Box>
         </Box>
         <Box className={styles['product__item__title']}>
